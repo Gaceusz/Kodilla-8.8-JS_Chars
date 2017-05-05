01abacf1cf757b6f7a06ab7c342309b432dcabde
Gaceusz <inglorion@op.pl>
2017-05-05 15:11:37 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-var text='Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
 console.log(text);
-var animal='zielone słonie';
+var animal = 'zielone słonie';
 var animalUpperCased = animal.toUpperCase();
 console.log(animalUpperCased);
 var textCharsAfter = text.replace('Papugi', animalUpperCased);
